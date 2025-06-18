854f15dc95526989a6fcc734b606a9a12990cc76
Jordan Pisani <jordanpisani@pm.me>
2025-06-18 00:23:57 -0400

    update scripts, fix typo

--- components/Tools/SoulLevelCalculator.vue
+++ components/Tools/SoulLevelCalculator.vue
@@ -1,16 +1,36 @@
 <script lang="ts">
+import { defineComponent } from "vue";
+const pageTitle = "Soul Level Calculator";
+
+export default defineComponent({
+  name: "SoulLevelCalculator",
+  setup() {
+    useSeoMeta({
+      title: pageTitle,
+      ogTitle: pageTitle,
+      twitterTitle: pageTitle,
+    });
+    // You can define computed, reactive, refs, etc. here
+    // For example:
+    // const currentLevel = ref(1)
+    // const soulsRequired = computed(() => currentLevel.value * 1000)
+
+    return {
+      // expose to template
+    };
+  },
+});
+
 export const meta = {
   title: "Soul Level Calculator",
   description: "Calculates number of souls needed to reach a specific level.",
 };
 </script>
 
-<script setup lang="ts"></script>
-
 <template>
   <UContainer>
     <section>
-      <h1>Soul Level Calulator</h1>
+      <h1>Soul Level Calculator</h1>
 
       <p>Calculates number of souls needed to reach a specific level.</p>
     </section>
